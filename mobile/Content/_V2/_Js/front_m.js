37a12716f754f175c96d5bc2015a6b08e6045b44
김인경 <kik87827@gmail.com>
2023-04-17 00:33:42 +0900

    marquee 수정

--- mobile/Content/_V2/_Js/front_m.js
+++ mobile/Content/_V2/_Js/front_m.js
@@ -1,6 +1,6 @@
 
 
-function popupShow(item){
+function popupShow(item,callback){
     const modalWrap = document.querySelectorAll(".modal-wrap");
     const bodyDom = document.querySelector("body");
     const targetItem = item;
@@ -9,6 +9,9 @@ function popupShow(item){
     });
     targetItem.classList.add("active");
     bodyDom.classList.add("touchDis");
+    if(callback){
+        callback();
+    }
     targetItem.querySelector(".btn-modal-close").addEventListener("click",(e)=>{
         e.preventDefault();
         item.classList.remove("active");
@@ -20,4 +23,32 @@ function popupHide(item){
     const targetItem = item;
     targetItem.classList.remove("active");
     bodyDom.classList.remove("touchDis");
+}
+
+
+function swiperMarqueeCall(){
+    let swiperMarquee = null;
+    if(swiperMarquee !== null){
+        swiperMarquee.update();
+        
+    }else{
+        swiperMarquee = new Swiper('.swiper-marquee', {
+            spaceBetween: 0,
+            centeredSlides: true,
+            speed: 1500,
+            freeMode: true,
+            autoplay: {
+                delay: 1,
+            },
+            loop: true,
+            slidesPerView: 'auto',
+            allowTouchMove: false,
+            disableOnInteraction: true
+        });
+    }
+    window.addEventListener("resize",()=>{
+        if(swiperMarquee !== null){
+            swiperMarquee.update();
+        }
+    });
 }
